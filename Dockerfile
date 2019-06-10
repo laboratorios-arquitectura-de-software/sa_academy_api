f5f376e0504d4bfb344eb6c1e64e923f7fdec811
Jhon Jairo <jjmuesesq@unal.edu.co>
2019-06-09 23:26:39 -0500

    subiendo api gateway

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM node:carbon-slim
+
+# Create app directory
+WORKDIR /git/academy-api
+
+# Install app dependencies
+COPY package.json /git/academy-api/
+RUN npm install
+
+# Bundle app source
+COPY . /git/academy-api/
+RUN npm run prepublish
+
+EXPOSE 5005
+
+CMD [ "npm", "run", "runServer" ]
+
---
+++ src/graphQLSchema.js
@@ -0,0 +1,47 @@
+import merge from 'lodash.merge';
+import GraphQLJSON from 'graphql-type-json';
+import { makeExecutableSchema } from 'graphql-tools';
+
+import { mergeSchemas } from './utilities';
+
+import {
+	studentsMutations,
+	studentsQueries,
+	studentsTypeDef
+} from './students/typeDefs';
+
+import {
+	coursesMutations,
+	coursesQueries,
+	coursesTypeDef
+} from './courses/typeDefs';
+
+import studentsResolvers from '/students/resolvers';
+import coursesResolvers from './courses/resolvers';
+
+// merge the typeDefs
+const mergedTypeDefs = mergeSchemas(
+	[
+		'scalar JSON',
+		studentsTypeDef,
+		coursesTypeDef
+	],
+	[
+		studentsQueries,
+		coursesQueries
+	],
+	[
+		studentsMutations,
+		coursesMutations
+	]
+);
+
+// Generate the schema object from your types definition.
+export default makeExecutableSchema({
+	typeDefs: mergedTypeDefs,
+	resolvers: merge(
+		{ JSON: GraphQLJSON }, // allows scalar JSON
+		studentsResolvers,
+		coursesResolvers
+	)
+});
